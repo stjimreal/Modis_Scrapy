aa48c5420145cc8b049f61f5059b3ee140cbe80e
liulijing@bupt.edu.cn <stjimreal@gmail.com>
2021-03-26 21:56:17 +0800

    修复一个读取配置文件的错误

--- cfg.py
+++ cfg.py
@@ -1,15 +1,8 @@
 '''
 Date: 2021-03-26 19:20:38
 LastEditors: LIULIJING
-LastEditTime: 2021-03-26 19:42:11
+LastEditTime: 2021-03-26 21:43:28
 '''
 import json
-import json.decoder
 
-Conf = dict()
-class ModisNsidcSpiderConfig:
-    def __init__(self, config_file='configuration.json'):
-        super().__init__()
-        with open(config_file) as f:
-            Conf = json.load(f)
-
+Conf = json.load(open('configuration.json'))
